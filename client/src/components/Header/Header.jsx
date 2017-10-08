8d1fad4842a0d120cdde98192ee17a789f116aed
vvanslee <vvanslee@gmail.com>
2017-10-08 18:42:06 -0500

    added header component

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () =>
+  <header className="header" >
+    <h1>
+        The New York Times Article Searcher
+    </h1>
+    <p>Search for and annotate articles of interest</p>
+  </header>;
+
+export default Header;
